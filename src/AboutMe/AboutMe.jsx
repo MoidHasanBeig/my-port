5f2dedb927e72c16d016f203413a38c174f4b4a9
MoidHasanBeig <mhbdgr8@gmail.com>
2020-04-01 07:13:50 +0530

    Change about me layout and make responsive

--- src/AboutMe/AboutMe.jsx
+++ src/AboutMe/AboutMe.jsx
@@ -7,8 +7,13 @@ function AboutMe() {
     <div className="about-me-section">
       <div className="about-filler" />
       <div className="about-heading">I design&emsp;&&emsp;I develop</div>
-      <div className="about-pic" />
-      <p className="about-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+      <p className="about-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+      <div className="about-pic-container">
+        <div className="about-pic-subcontainer">
+          <div className="about-pic" />
+        </div>
+        <hr/>
+      </div>
     </div>
   );
 
