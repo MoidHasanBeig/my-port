a6ef29becf5edfcef16981744f2b59bd3b612888
MoidHasanBeig <mhbdgr8@gmail.com>
2020-05-05 15:46:39 +0530

    Visual revamp a bit

--- src/AboutMe/AboutMe.jsx
+++ src/AboutMe/AboutMe.jsx
@@ -7,11 +7,31 @@ function AboutMe(props) {
   return (
     <div className={`about-me-container ${props.dark && "invert"}`}>
       <div className={`about-me-section section ${props.dark && "no-bg"}`} id="about-me">
-        <div className="about-heading">I design&emsp;&&emsp;I develop</div>
-        <p className="about-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-        <div className={`about-pic-container ${props.dark && "invert"}`}>
-          <div className="about-pic-subcontainer">
-            <div className="about-pic" />
+        <div className="about-heading">I design<br />& I develop</div>
+        <div className="details-container">
+          <div className="individual-intro cod-intro">
+            <div className={`about-pic-container ${props.dark && "invert"}`}>
+              <div className="about-pic-subcontainer">
+                <div className="about-pic cod-pic" />
+              </div>
+            </div>
+            <p className="about-text">quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+          </div>
+          <div className="individual-intro des-intro">
+            <div className={`about-pic-container ${props.dark && "invert"}`}>
+              <div className="about-pic-subcontainer">
+                <div className="about-pic des-pic" />
+              </div>
+            </div>
+            <p className="about-text">quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+          </div>
+          <div className="individual-intro pla-intro">
+            <div className={`about-pic-container ${props.dark && "invert"}`}>
+              <div className="about-pic-subcontainer">
+                <div className="about-pic pla-pic" />
+              </div>
+            </div>
+            <p className="about-text">quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
           </div>
         </div>
       </div>
