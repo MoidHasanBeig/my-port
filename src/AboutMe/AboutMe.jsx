a84b446a260f257ba78fe8de9aa169565e74ba28
MoidHasanBeig <mhbdgr8@gmail.com>
2020-06-29 09:33:40 +0530

    Code refactoring ABOUT ME

--- src/AboutMe/AboutMe.jsx
+++ src/AboutMe/AboutMe.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './aboutme.styles.scss';
 import './aboutme.responsive.scss';
-import FooterQuote from '../components/FooterQuote/FooterQuote';
 
 function AboutMe(props) {
   return (
@@ -11,31 +10,24 @@ function AboutMe(props) {
         <div className="details-container">
           <div className="individual-intro cod-intro">
             <div className={`about-pic-container ${props.dark && "invert"}`}>
-              <div className="about-pic-subcontainer">
-                <div className="about-pic cod-pic" />
-              </div>
+              <div className="about-pic cod-pic" />
             </div>
             <p className="about-text">quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
           </div>
           <div className="individual-intro des-intro">
             <div className={`about-pic-container ${props.dark && "invert"}`}>
-              <div className="about-pic-subcontainer">
-                <div className="about-pic des-pic" />
-              </div>
+              <div className="about-pic des-pic" />
             </div>
             <p className="about-text">quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
           </div>
           <div className="individual-intro pla-intro">
             <div className={`about-pic-container ${props.dark && "invert"}`}>
-              <div className="about-pic-subcontainer">
-                <div className="about-pic pla-pic" />
-              </div>
+              <div className="about-pic pla-pic" />
             </div>
             <p className="about-text">quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
           </div>
         </div>
       </div>
-      <FooterQuote text="&quot;Whatever you do, do it well.&quot; – Walt Disney" />
     </div>
   );
 
