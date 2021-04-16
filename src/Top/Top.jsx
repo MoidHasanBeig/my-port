08a5f95330949d243b39b32efca0d4e59bed1a9a
MoidHasanBeig <MoidHasanBeig@github.com>
2021-04-15 22:28:00 -0400

    Remove constellation and mode button placeholder

--- src/Top/Top.jsx
+++ src/Top/Top.jsx
@@ -13,7 +13,6 @@ function Top(props) {
   return (
     <div className={`top-section section ${props.dark && "night-sky"}`} id="top">
       <div className="top-additional-images" />
-      <div className={`${!props.dark && "show-btn-location"}`}/>
       <div className={`light-source ${props.dark && "moon"} ${isSunAnimate && "animate-sunset"}`} onAnimationEnd={() => setIsSunAnimate(false)}/>
       <div className="speech-container">
         <div className="speech-bubble" />
@@ -22,7 +21,6 @@ function Top(props) {
       <div className="glob-container">
         <div className={`globe ${props.dark && "night-colors"}`} />
       </div>
-      <div className={`${!props.dark && "hide"} constellation`} />
       <div className={`${props.dark && "shooting-star"}`} />
       <div className={`seamless-clouds ${props.dark && "invert"}`} />
     </div>
